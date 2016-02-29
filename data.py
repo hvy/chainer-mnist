a3f5cdecdbbe7ebfc5fb144c05cd1c6b38d0935e
hvy <hiroyuki.vincent.yamazaki@gmail.com>
2016-02-29 10:40:35 +0000

    Add documentation to MNIST data loader

--- data.py
+++ data.py
@@ -1,10 +1,9 @@
 import os
+import sys
 import gzip
 import numpy as np
 import six
 from six.moves.urllib import request
-from common import Network, Dimensions
-
 
 # MNIST source URLs
 base_url= 'http://yann.lecun.com/exdb/mnist'
@@ -13,21 +12,18 @@ train_labels = 'train-labels-idx1-ubyte.gz'
 test_images = 't10k-images-idx3-ubyte.gz'
 test_labels = 't10k-labels-idx1-ubyte.gz'
 
-# MNIST image properties. Total number of images = 70000
-num_channels = 1
-num_train = 60000 
-num_test = 10000
-
 
 def download_mnist():
-    print('Downloading MNIST...')
+    """ Download the MNIST dataset from Yann LeCun's homepage.
+
+    """
     download_file('{}/{}'.format(base_url, train_images), train_images)
     download_file('{}/{}'.format(base_url, train_labels), train_labels)
     download_file('{}/{}'.format(base_url, test_images), test_images)
     download_file('{}/{}'.format(base_url, test_labels), test_labels)
 
+
 def download_file(source_file, target_file):
-    # TODO Use os.path.join
     if not os.path.exists(target_file):
         print('Downloading {}...'.format(source_file))
         request.urlretrieve(source_file, target_file)
@@ -36,6 +32,12 @@ def download_file(source_file, target_file):
 
 
 def create_from_mnist(filename, shape, size):
+    """ Save an MNIST dataset representational file to disk with the name
+    specified by the parameter.
+
+    """
+    num_train = 60000 
+    num_test = 10000
     if not os.path.exists(filename):
         print('Processing MNIST to create {}, this might take a while...'.format(filename))
         data_train, target_train = load_reshape(train_images, train_labels, num_train, shape, size)
@@ -48,16 +50,11 @@ def create_from_mnist(filename, shape, size):
     else:
         print('Found {}'.format(filename))
 
-#     # Load the cached data
-#     print('Loading file {}'.format(filename))
-#     with open(filename, 'rb') as mnist_pickle:
-#         mnist = six.moves.cPickle.load(mnist_pickle)
-#         print('Successfully loaded file')
-# 
-#     return mnist
-# 
 
 def load_reshape(images, labels, num, shape, size):
+    """ Load the MNIST gzip files from disk and reshape the data.
+
+    """
     shape = tuple([num, *shape])   
     data = np.zeros(num * size, dtype=np.uint8).reshape(shape)
     target = np.zeros(num, dtype=np.uint8).reshape((num, ))
@@ -84,7 +81,27 @@ def load_reshape(images, labels, num, shape, size):
 
 
 if __name__ == '__main__':
-    print('Preparing the MNIST data...')
+    """ Download the MNIST dataset from Yann LeCun's homepage, reshape the data 
+    to 1-dimensional data for MLP models and 2-dimensional data for CNN models.
+    A .pkl file is created in this directory for each of the two types of models.
+
+    The dataset contains a total of 70000 images, consisting of  60000 training 
+    samples and 10000 test samples.
+
+    Note that the data only contains single grayscale channels and not three 
+    as in RGB images.
+
+    """ 
+
+    mlp_filename = 'mnist_1dim.pkl'
+    cnn_filename = 'mnist_2dim.pkl'
+
+    if os.path.exists(mlp_filename) and os.path.exists(cnn_filename):
+        print('The MNIST dataset is already downloaded and reshaped: {}, {}'.format(mlp_filename, cnn_filename))
+        print('To download them again, delete the files and rerun this script')
+        sys.exit()
+    
+    print('Preparing the MNIST dataset...')
     
     download_mnist()
 
